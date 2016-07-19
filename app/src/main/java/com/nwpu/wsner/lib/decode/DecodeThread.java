1c15a07945fb3991a01e9f28422513334d315068
lmy <624571192@qq.com>
2016-07-19 17:42:27 +0800

    LMY zbar

---
+++ app/src/main/java/com/nwpu/wsner/lib/decode/DecodeThread.java
@@ -0,0 +1,42 @@
+package com.nwpu.wsner.lib.decode;
+
+import android.os.Handler;
+import android.os.Looper;
+
+
+import com.nwpu.wsner.lib.CaptureActivity;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * 描述: 解码线程
+ */
+final class DecodeThread extends Thread {
+
+	CaptureActivity activity;
+	private Handler handler;
+	private final CountDownLatch handlerInitLatch;
+
+	DecodeThread(CaptureActivity activity) {
+		this.activity = activity;
+		handlerInitLatch = new CountDownLatch(1);
+	}
+
+	Handler getHandler() {
+		try {
+			handlerInitLatch.await();
+		} catch (InterruptedException ie) {
+			// continue?
+		}
+		return handler;
+	}
+
+	@Override
+	public void run() {
+		Looper.prepare();
+		handler = new DecodeHandler(activity);
+		handlerInitLatch.countDown();
+		Looper.loop();
+	}
+
+}
--- app/src/main/java/com/nwpu/wsner/ui/fragments/FragmentManager.java
+++ app/src/main/java/com/nwpu/wsner/ui/fragments/FragmentManager.java
@@ -1,32 +1,43 @@
 package com.nwpu.wsner.ui.fragments;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
-
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import com.nwpu.wsner.R;
+import com.nwpu.wsner.lib.CaptureActivity;
+
 /**
  * Created by Michal Bialas on 19/07/14.
  */
-public class FragmentTwo extends Fragment {
+public class FragmentManager extends Fragment {
 
     @InjectView(R.id.circleLayout)
     LinearLayout circleLayout;
-
+    private Button btn;
+    private Context mContext;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup containter,
             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_two, containter, false);
         ButterKnife.inject(this, view);
+        mContext=getActivity();
         ((GradientDrawable) circleLayout.getBackground())
                 .setColor(getResources().getColor(R.color.material_pink));
+        initView(view);
         return view;
+    }
+    public void initView(View view){
+        btn=(Button)view.findViewById(R.id.scaner);
+        btn.setOnClickListener(mListener);
     }
 
     @Override
@@ -34,7 +45,25 @@ public class FragmentTwo extends Fragment {
         super.onDestroyView();
         ButterKnife.reset(this);
     }
+/**
+ *    ADD onClickListener Here
+ */
+ private View.OnClickListener mListener= new View.OnClickListener(){
 
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
 
+        switch (v.getId()){
+            case R.id.scaner:
+            startActivity(new Intent(mContext, CaptureActivity.class));
+                break;
+        }
+    }
+};
 
 }
