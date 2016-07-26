ac3854757b623a5b41cb41a08fd25fc0bbc44cc0
lmy <624571192@qq.com>
2016-07-26 22:35:08 +0800

    LMY productManager listView

--- app/src/main/java/com/nwpu/wsner/constants/Url.java
+++ app/src/main/java/com/nwpu/wsner/constants/Url.java
@@ -4,7 +4,7 @@ package com.nwpu.wsner.constants;
  * Created by zjy on 16/7/18.
  */
 public class Url {
-    private static final String IP_PORT="http://10.13.33.91:8080/";
+    private static final String IP_PORT="http://10.13.33.91:8088/";
     private static final String BASE_URL=IP_PORT+"";
 
 
@@ -12,13 +12,13 @@ public class Url {
     public static final String SIGIN_URL=BASE_URL+"";
     public static final String WETHER_URL="http://php.weather.sina.com.cn/xml.php?city=%CE%F7%B0%B2&password=DJOYnieT8234jlsK&day=0";
 
-    private static final String PRODUCT_ADD_URL = "http://localhost:8088/SmartCabinet/product/productScan?";
+    private static final String PRODUCT_ADD_URL = IP_PORT+"SmartCabinet/product/productScan?";
 
     /**
      添加物品 * http://ip:port/SmartCabinet/product?barcode=xx&productionDate=xx&productionValid=xxx&username=xxx
      */
     public static String addProdcutUrl(String barcode,String productionDate,String username ) {
-        return PRODUCT_ADD_URL + "barcode=" + barcode + "productionDate=" + productionDate + "username=" + username;
+        return PRODUCT_ADD_URL + "barcode=" + barcode + "&productionDate=" + productionDate + "&username=" + username;
     }
 
 }
